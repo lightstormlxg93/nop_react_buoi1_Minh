591f89d630340789d2e4fdadbb673e74499cdc3c
Minh <lightstormlxag93@gmail.com>
2024-02-05 11:11:31 +0700

    trans class o classname

--- src/BaiTapLayoutComponent/Header.jsx
+++ src/BaiTapLayoutComponent/Header.jsx
@@ -3,26 +3,20 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="container px-lg-5"
-             style={{textAlign:"right"}}
-            >
-                <a class="navbar-brand" href="#!">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="flex-row-reverse collapse navbar-collapse" id="navbarSupportedContent"
-                    
-                >
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0" 
-                   
-                    >
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!"
-                        >Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container px-lg-5" style={{textalign: "right"}}>
+    <a className="navbar-brand" href="#!">Start Bootstrap</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon" /></button>
+    <div className="flex-row-reverse collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
+        <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
+        <li className="nav-item"><a className="nav-link" href="#!">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
         
     )
   }
